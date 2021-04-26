fe956bb58d1cc317e79747a76b2c0daee9e29921
Ujjwal <ujjwal.jain1012@gmail.com>
2021-04-25 21:27:58 -0700

    new server IP

--- frontend/src/reducers/HistoryReducers.js
+++ frontend/src/reducers/HistoryReducers.js
@@ -22,7 +22,7 @@ export const history = (payload) => async (dispatch, getState) => {
   console.log(payload);
   await axios
     .post(
-      "http://54.153.78.74:3010/history",
+      "http://3.101.73.198:3010/history",
       { userId:  userId },
       {
         headers: {
